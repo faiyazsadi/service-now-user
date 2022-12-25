beecf808eb393163b9ed010787741df2204fbecb
faiyazsadi <sadi1807073@stud.kuet.ac.bd>
2022-12-25 12:56:08 +0600

    pre merge with userapp

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -3,3 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 final FirebaseAuth fAuth = FirebaseAuth.instance;
 
 User? currentFirebaseuser;
+
+var currAcceptTime = "", prevAcceptTime = "";
+bool requestDisabled = false;
